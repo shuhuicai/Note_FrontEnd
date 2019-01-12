911feaedcf150eba314eacef13b719a4a9d78c21
shuhui <1572316380@qq.com>
2019-01-12 22:23:16 +0800

    引入v-contextmenu组件

--- src/components/Tag/tagMenu.vue
+++ src/components/Tag/tagMenu.vue
@@ -1,5 +1,12 @@
-<template>
+<template xmlns:v-contextmenu="http://www.w3.org/1999/xhtml">
   <div>
+    <v-contextmenu ref="contextmenu">
+      <v-contextmenu-item>菜单1</v-contextmenu-item>
+      <v-contextmenu-item>菜单2</v-contextmenu-item>
+      <v-contextmenu-item>菜单3</v-contextmenu-item>
+    </v-contextmenu>
+    
+    <div v-contextmenu:contextmenu>click here</div>
   </div>
 </template>
 
