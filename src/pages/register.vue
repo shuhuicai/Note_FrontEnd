599d04c1aaa741d00281c3b60bfec9778af81bbb
shuhui <1572316380@qq.com>
2019-04-21 14:17:56 +0800

    完成注册功能

--- src/pages/register.vue
+++ src/pages/register.vue
@@ -2,12 +2,12 @@
   <div class="register">
     <div class="inputBox">
       <p>注册</p>
-      <p>邮箱<input v-model="email" type="email"/></p>
+      <p>邮箱<input v-model="email" type="text"/></p>
       <p>密码<input v-model="password" type="password"/></p>
       <p>验证码<input v-model="verify_input" type="text"/>
         <button type="button" @click="getVerify">获取验证码</button>
       </p>
-      <button type="button">注册</button>
+      <button type="button" @click="registerReq">注册</button>
     </div>
   </div>
 </template>
@@ -33,7 +33,95 @@
           });
           return;
         }
+        if (!this.checkEmail()) {
+          return;
+        }
+        fetch(this.constant.serverURL + "/user/sendVerifyCode", {
+          body: "address=" + this.email.trim(),
+          cache: 'no-cache',
+          credentials: 'same-origin',
+          method: 'POST',
+          mode: 'cors',
+          redirect: 'follow',
+          referrer: 'no-referrer',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            Accept: "application/json"
+          }
+        }).then(response => {
+            response.json().then((data) => {
+              this.verify = data;
+            })
+          },
+          response => {
+          
+          })
         
+      },
+      
+      registerReq() {
+        if (this.email.trim() === "" || this.password.trim() === "" || this.verify_input.trim() === "") {
+          this.$message({
+            message: '请输入完整的信息',
+            type: 'warning'
+          });
+          return;
+        }
+        if (this.verify != this.verify_input.trim()) {
+          this.$message({
+            message: '验证码错误',
+            type: 'warning'
+          });
+        } else {
+          fetch(this.constant.serverURL + "/user/addUser", {
+            body: JSON.stringify({
+              "account": this.email.trim(),
+              "password": this.password.trim(),
+              "role": 1
+            }),
+            cache: 'no-cache',
+            credentials: 'include',
+            method: 'POST',
+            mode: 'cors',
+            redirect: 'follow',
+            referrer: 'no-referrer',
+            headers: {
+              "Content-Type": 'application/json;charset=UTF-8',
+              Accept: "application/json"
+            }
+          }).then(response => {
+              response.json().then((data) => {
+                if (data) {
+                  this.$notify({
+                    title: '成功',
+                    message: '注册成功',
+                    type: 'success'
+                  });
+                  this.$router.push({path: '/login'});
+                } else {
+                  this.$notify.error({
+                    title: '提示',
+                    message: '注册失败',
+                  });
+                }
+              })
+            },
+            response => {
+            
+            });
+        }
+      },
+      //检验邮箱格式是否正确
+      checkEmail() {
+        var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+        if (!reg.test(this.email.trim())) {
+          this.$message({
+            message: '邮箱格式不正确',
+            type: 'warning'
+          });
+          return false;
+        }
+        return true;
       }
     }
   }
